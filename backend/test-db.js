bb1a06ad0cc2dc24d4d6cb1481d9c24a991580d0
Manojya1701 <manojya.guruju@gmail.com>
2026-08-15 13:34:26 +0530

    Clean up legacy prototype files and redundant dependencies

--- backend/test-db.js
+++
@@ -1,9 +0,0 @@
-const { protectValue } = require("./hash");
-
-console.log(
-  protectValue("manu@gmail.com")
-);
-
-console.log(
-  protectValue("manu@gmail.com")
-);
--- view-db.js
+++
@@ -1,7 +0,0 @@
-const db = require("./database");
-
-const records = db.prepare(`
-SELECT * FROM privacy_records
-`).all();
-
-console.table(records);
